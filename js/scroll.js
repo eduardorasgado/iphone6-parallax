0d0e5e11393eff8054883c8cd7dc2406a1bebfad
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2018-04-20 06:24:31 -0500

    Se modifica movimiento en scroll.js, se agrega footer con bootstrap3 y se agrega fontAwesome

--- js/scroll.js
+++ js/scroll.js
@@ -3,5 +3,11 @@ const $mask = document.getElementById('mask');
 
 window.addEventListener('scroll', function(e){
 	console.log(window.scrollY);
-	$mask.style.backgroundPosition = window.scrollY/5+'px';
+		//limites en la pantalla para mostrar cierto rango del backgrnd
+		if(window.scrollY<= 583 && window.scrollY>=400){
+			//los 210px del header menos la pos inicial del backgrnd menos scrollY
+			$mask.style.backgroundPosition = 310-window.scrollY-57+'px';
+		}
+		var position = 310-window.scrollY-57;
+		$iphone.style.transform = 'rotate('+position*0.02+'deg)';
 });
